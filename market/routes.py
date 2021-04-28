38b796a42ca5018562327d352ce67e44ac069180
Renan <renaner123@gmail.com>
2021-04-28 20:46:24 -0300

    Project Restructure, Packages

---
+++ market/routes.py
@@ -0,0 +1,18 @@
+from market import app
+from flask import render_template
+from market.models import Item
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+""" @app.route('/about/<username>')
+def about(username):
+    return f'<h1> This is the about page of {username}' """
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+
+    return render_template('market.html',items=items)
